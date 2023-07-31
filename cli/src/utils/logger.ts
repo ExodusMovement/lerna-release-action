3dfb604d23849bc15e365ee4c609c5eaf3b1094d
Jan Wendland <jan.we@exodus.io>
2023-07-31 20:18:59 +0200

    feat: pipe stdout/err through pino

--- cli/src/utils/logger.ts
+++ cli/src/utils/logger.ts
@@ -6,4 +6,29 @@ const logger = pino({
   },
 })
 
+const isDebug = (message: string) => message.includes('::debug::')
+const clean = (message: string) => message.replace('::debug::', '')
+
+process.stdout.write = (buffer) => {
+  const message = buffer.toString().trim()
+
+  if (isDebug(message)) {
+    logger.debug(clean(message))
+    return true
+  }
+
+  logger.info(message)
+  return true
+}
+
+process.stderr.write = (buffer) => {
+  const message = buffer.toString().trim()
+
+  if (message) {
+    logger.error(message)
+  }
+
+  return true
+}
+
 export default logger
