7fb08e5ad2591099a3037279ab60210d9ac03f93
Jan W <jan.we@exodus.io>
2025-05-29 14:28:41 +0200

    fix(publish): ensure unscoped packages are tagged (#62)
    * fix(publish): ensure unscoped packages are tagged
    
    * build: update dist
    
    * fix: use correct part when no packages published

--- src/publish/extract-tags.spec.ts
+++ src/publish/extract-tags.spec.ts
@@ -6,6 +6,7 @@ describe('extractTags', () => {
     expect(extractTags(PUBLISH_OUTPUT)).toEqual([
       '@exodus/arkham-asylum@2.1.7-alpha.1',
       '@exodus/wayne-manor@1.9.12',
+      'secure-container@3.0.0',
     ])
   })
 })
